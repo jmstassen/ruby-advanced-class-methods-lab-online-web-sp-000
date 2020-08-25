8a24b59f80de9f84bb7b778eaf32b8dc7e2f277f
John Stassen <john.stassen@gmail.com>
2020-08-25 15:44:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,4 +55,13 @@ class Song
     song
   end
 
+  def self.create_from_filename(filename)
+    new_file = filename.split(" - ") #'initializes and saves a song and artist_name based on the filename format'
+    artist_name = new_file[0]
+    song_name = new_file[1].gsub(".mp3","")
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
 end
